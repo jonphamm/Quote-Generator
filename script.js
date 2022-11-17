b129dee2e4b1e8d6882842cdee713d480478dfe1
jonphamm <jonpham09@gmail.com>
2022-11-17 12:58:34 -0500

    Edit responsiveness and appearance

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-const getNewQuote = async () => {
-    // api for random quotes
-    var url = "https://type.fit/api/quotes";
+const text = document.getElementById("quote");
+const author = document.getElementById("author");
+const tweetButton = document.getElementById("tweet");
 
-    // fetch the data from api, fetch() method returns a promise, which is handled by the await keyword
-    const response = await fetch(url);
+const getNewQuote = async() => {
+    //api for quotes
+    var url = "https://type.fit/api/quotes";    
+
+    // fetch the data from api
+    const response=await fetch(url);
+
+    // convert response to json and store it in quotes array
+    const allQuotes = await response.json();
+
+    // generates a random number between 0 and the length of the quotes array
+    const index = Math.floor(Math.random() * allQuotes.length);
+
+    // store the quote present at the randomly generated index
+    const quote = allQuotes[index].text;
+    
+    // store the author of the respective quote
+    const auth = allQuotes[index].author;
+
+    if(auth == null) {
+        author = "Anonymous";
+    }
+ 
+    // function to dynamically display the quote and the author
+    text.innerHTML = quote;
+    author.innerHTML = "- " + auth;
+
+    // tweet the quote
+    tweetButton.href = "https://twitter.com/intent/tweet?text=" + quote + " - " + auth;
+
 }
+
+getNewQuote();
